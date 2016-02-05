8d7f0ad94c498e5e76b4166eb613fc4be09bbc64
webmalc <webmalc@gmail.com>
2016-02-05 18:15:41 +0300

    Organization admin

--- users/admin.py
+++ users/admin.py
@@ -2,20 +2,73 @@ from django.contrib.auth.admin import admin, UserAdmin
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from allauth.account.models import EmailAddress
-from users.models import Profile
+from users.models import Profile, Organization, PartnershipOrder
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    """
+    Organization admin interface
+    """
+    list_display = ('type', 'name', 'created_at', 'created_by')
+    list_display_links = ('type', 'name')
+    search_fields = ('name', 'type')
+    list_filter = ('type', 'created_at')
+    fieldsets = (
+        ('General', {
+            'fields': ('type', 'name')
+        }),
+    )
+
+
+class OrganizationInline(admin.TabularInline):
+    """
+    User organization admin interface
+    """
+    model = Organization
+
+
+class PartnershipOrderAdmin(admin.ModelAdmin):
+    """
+    PartnershipOrder admin interface
+    """
+    list_display = ('get_full_name', 'created_at', 'created_by', 'status')
+    list_display_links = ('get_full_name',)
+    search_fields = ('last_name',)
+    list_filter = ('created_at', )
+    raw_id_fields = ('city', 'organization')
+    fieldsets = (
+        ('General', {
+            'fields': ('last_name', 'first_name', 'patronymic', 'type', 'status')
+        }),
+        ('Contacts', {
+            'fields': ('phone', 'city', 'organization')
+        }),
+        ('Other', {
+            'fields': ('experience', 'comment')
+        })
+    )
 
 
 class ProfileInline(admin.StackedInline):
+    """
+    Profile admin interface
+    """
     model = Profile
     verbose_name_plural = 'Profile'
-    raw_id_fields = ("city",)
+    raw_id_fields = ('city', 'organization')
 
 
 class EmailsInline(admin.TabularInline):
+    """
+    User emails admin interface
+    """
     model = EmailAddress
 
 
 class MyUserAdmin(UserAdmin):
+    """
+    User admin interface
+    """
     list_display = UserAdmin.list_display + ('last_login',)
     list_filter = UserAdmin.list_filter + ('last_login',)
     inlines = [
@@ -47,3 +100,5 @@ class MyUserAdmin(UserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(PartnershipOrder, PartnershipOrderAdmin)
--- users/models.py
+++ users/models.py
@@ -18,22 +18,45 @@ class User(BaseUser):
             return '%s' % self.username
 
 
+class Organization(CommonInfo):
+    """
+    Organization model
+    """
+
+    TYPES = {
+        'ooo': 'ООО',
+        'zao': 'ЗАО',
+        'oao': 'ОАО',
+        'ip': 'ИП ',
+        'other': 'Другое'
+    }
+    type = models.CharField(max_length=20, choices=TYPES.items())
+    name = models.CharField(max_length=255)
+
+    def get_type_name(self):
+        return self.TYPES[self.type]
+
+    def __str__(self):
+        return '{} {}'.format(self.get_type_name(), self.name)
+
+
 class PartnershipCommonInfo(models.Model):
     """
     Base Partnership information
     """
-    TYPES = [
-        ('agent', 'Представитель агентства'),
-        ('realtor', 'Частный агент/Риэлтор'),
-        ('hotel_worker', 'Представитель отеля'),
-        ('hotel_owner', 'Владелец отеля'),
-        ('other', 'Другое'),
-    ]
+    TYPES = {
+        'agent': 'Представитель агентства',
+        'realtor': 'Частный агент/Риэлтор',
+        'hotel_worker': 'Представитель отеля',
+        'hotel_owner': 'Владелец отеля',
+        'other': 'Другое',
+    }
 
     patronymic = models.CharField(max_length=50, null=True, blank=True)
-    type = models.CharField(max_length=50, choices=TYPES)
+    type = models.CharField(max_length=50, choices=sorted(TYPES.items()))
     phone = models.CharField(max_length=30)
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=False)
+    organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True, blank=False)
     experience = models.PositiveSmallIntegerField()
 
     class Meta:
@@ -44,17 +67,20 @@ class PartnershipOrder(CommonInfo, PartnershipCommonInfo):
     """
     Base Partnership information
     """
-    STATUSES = [
-        ('new', 'Ожидает обработки'),
-        ('in_work', 'Обрабатывается'),
-        ('approved', 'Принята'),
-        ('canceled', 'Отклонена')
-    ]
+    STATUSES = {
+        'new': 'Ожидает обработки',
+        'in_work': 'Обрабатывается',
+        'approved': 'Принята',
+        'canceled': 'Отклонена'
+    }
 
     first_name = models.CharField(_('first name'), max_length=30, blank=False)
     last_name = models.CharField(_('last name'), max_length=30, blank=False)
     comment = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUSES, default='new')
+    status = models.CharField(max_length=20, choices=STATUSES.items(), default='new')
+
+    def get_full_name(self):
+        return "{} {} {}".format(self.last_name, self.first_name, self.patronymic)
 
 
 class Profile(PartnershipCommonInfo):
