a2788ac373df15f54838b5d54a85c04e4d7da0c8
webmalc <webmalc@gmail.com>
2016-02-05 22:58:20 +0300

    Has group filter fix

--- hh/templatetags/has_group.py
+++ hh/templatetags/has_group.py
@@ -6,6 +6,11 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    group = Group.objects.get(name=group_name)
-    return True if group in user.groups.all() else False
+    try:
+        group = Group.objects.filter(name=group_name)
+        return True if group in user.groups.all() else False
+    except Group.DoesNotExist:
+        return False
+
+
 
