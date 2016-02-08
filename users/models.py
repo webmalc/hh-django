1a2651703edab69c936c36e9c7937571f0f2f721
webmalc <webmalc@gmail.com>
2016-02-08 16:37:53 +0300

    Fix choices in models

--- users/models.py
+++ users/models.py
@@ -24,37 +24,34 @@ class Organization(CommonInfo):
     Organization model
     """
 
-    TYPES = {
-        'ooo': 'ООО',
-        'zao': 'ЗАО',
-        'oao': 'ОАО',
-        'ip': 'ИП ',
-        'other': 'Другое'
-    }
-    type = models.CharField(max_length=20, choices=TYPES.items())
+    TYPES = (
+        ('ooo', 'ООО',),
+        ('zao', 'ЗАО',),
+        ('oao', 'ОАО',),
+        ('ip', 'ИП ',),
+        ('other', 'Другое')
+    )
+    type = models.CharField(max_length=20, choices=TYPES)
     name = models.CharField(max_length=255)
 
-    def get_type_name(self):
-        return self.TYPES[self.type]
-
     def __str__(self):
-        return '{} {}'.format(self.get_type_name(), self.name)
+        return '{} {}'.format(self.get_type_display(), self.name)
 
 
 class PartnershipCommonInfo(models.Model):
     """
     Base Partnership information
     """
-    TYPES = {
-        'agent': 'Представитель агентства',
-        'realtor': 'Частный агент/Риэлтор',
-        'hotel_worker': 'Представитель отеля',
-        'hotel_owner': 'Владелец отеля',
-        'other': 'Другое',
-    }
+    TYPES = (
+        ('agent', 'Представитель агентства'),
+        ('realtor', 'Частный агент/Риэлтор'),
+        ('hotel_worker', 'Представитель отеля'),
+        ('hotel_owner', 'Владелец отеля'),
+        ('other', 'Другое'),
+    )
 
     patronymic = models.CharField(max_length=50, null=True, blank=True)
-    type = models.CharField(max_length=50, choices=sorted(TYPES.items()))
+    type = models.CharField(max_length=50, choices=TYPES)
     phone = models.CharField(max_length=30)
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=False)
     organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True, blank=False)
@@ -78,17 +75,17 @@ class PartnershipOrder(CommonInfo, PartnershipCommonInfo):
     """
     Base Partnership information
     """
-    STATUSES = {
-        'new': 'Ожидает обработки',
-        'in_work': 'Обрабатывается',
-        'approved': 'Принята',
-        'canceled': 'Отклонена'
-    }
+    STATUSES = (
+        ('new', 'Ожидает обработки'),
+        ('in_work', 'Обрабатывается'),
+        ('approved', 'Принята'),
+        ('canceled', 'Отклонена')
+    )
 
     first_name = models.CharField(_('first name'), max_length=30, blank=False)
     last_name = models.CharField(_('last name'), max_length=30, blank=False)
     comment = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUSES.items(), default='new')
+    status = models.CharField(max_length=20, choices=STATUSES, default='new')
 
     def get_full_name(self):
         return "{} {} {}".format(self.last_name, self.first_name, self.patronymic)
