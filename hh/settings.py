3e7aa7d81d28d179a76e267aeb56c4989e24d9c2
webmalc <webmalc@gmail.com>
2016-02-10 14:07:08 +0300

    Compressor fix

--- hh/settings.py
+++ hh/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
 )
 
 FIXTURE_DIRS = (
